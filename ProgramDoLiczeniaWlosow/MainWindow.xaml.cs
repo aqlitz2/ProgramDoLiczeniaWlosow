a5915c62b7d4b76719d66b1bd1fe052cda8bacb8
aqlitz <hange@DESKTOP-SD1B6EQ>
2024-11-01 15:27:12 +0100

    Dodanie domyslnych wartości oraz deklaracja prywatnego pola

--- ProgramDoLiczeniaWlosow/MainWindow.xaml.cs
+++ ProgramDoLiczeniaWlosow/MainWindow.xaml.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Windows;
+using HairCalculator;
+
+namespace HairCalculator
+{
+    public partial class MainWindow : Window
+    {
+        private Glowa _glowa;
+
+        public MainWindow()
+        {
+
+        }
+    }
+}
--- ProgramDoLiczeniaWlosow/glowa.cs
+++ ProgramDoLiczeniaWlosow/glowa.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace HairCalculator
+{
+    public class Glowa
+    {
+        private const double DomyslnaGestosc = 100;
+        private const double DomyslnyObwod = 55;
+        private const double DomyslnaWysokoscCzola = 15;
+
+
+
+    }
+}

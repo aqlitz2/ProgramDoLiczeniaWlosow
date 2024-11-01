648d52419248a0ae5f60bd9a923d0df54f6d76c4
aqlitz <hange@DESKTOP-SD1B6EQ>
2024-11-01 15:31:23 +0100

    Dodanie publicznych wlasciowsci pokazujacych dane wejsciowe do obliczeń

--- ProgramDoLiczeniaWlosow/glowa.cs
+++ ProgramDoLiczeniaWlosow/glowa.cs
@@ -6,7 +6,9 @@
         private const double DomyslnyObwod = 55;
         private const double DomyslnaWysokoscCzola = 15;
 
-
+        public double Gestosc { get; set; }
+        public double ObwodGlowy { get; set; }
+        public double WysokoscCzola { get; set; }
 
     }
 }
